878bd076b12cd78e1850f4e4c54f967f02b7d797
Celsito <cmiranda16@stu.psm.edu>
2021-02-12 16:33:25 -0400

    Update simSIRmultiPopSingleIwithICUcomp.R
    Back to original script, Parameters of Puerto Rico population did not worked.

--- simSIRmultiPopSingleIwithICUcomp.R
+++ simSIRmultiPopSingleIwithICUcomp.R
@@ -29,10 +29,10 @@ simulateSIRmultiPopICUcompartment=function(R0default,  R0init, popParamsIn, rest
   npop=nrow(popParamsIn)
   
   #hard coded parameters"
-  I=930000 #100000 #total number of infected individuals
+  I=100000 #total number of infected individuals
   year=365 #year
   recoverytime=14 #recovery time in days. Taken to be 14 day
-  N=totalPop=3100000 #324356000 #US population, last census
+  N=totalPop=324356000 #US population, last census
   
   
   iif=1:free.bracket #index of the free age bracket
